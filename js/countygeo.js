c2fc385551bdab3015eb880a64d708b4f1db334f
An Bui <hvbuianbui@gmail.com>
2020-04-09 14:30:08 -0700

    update 2:30

--- js/countygeo.js
+++ js/countygeo.js
@@ -1,4 +1,7 @@
-let countyMap = L.map("countymap").setView([39.8283, -98.5795], 5);
+
+
+
+let countyMap = L.map("countymap").setView([39.8283, -98.5795], 3);
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
